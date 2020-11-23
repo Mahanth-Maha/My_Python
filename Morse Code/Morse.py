5d774935f9499d96877b7c41903a92542a05e1ae
Mahanth-Maha <mahanthysree@gmail.com>
2020-11-23 18:50:45 +0530

    Defined the Alphabetic to Morse A2m() function

--- Morse Code/Morse.py
+++ Morse Code/Morse.py
@@ -2,16 +2,29 @@
 
 MC = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..']
 AP = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-k=-1
+
+def A2M(string):
+    k=[]
+    mco = ''
+    string = string.upper()
+    listi = list(string)
+    print(listi)
+    for l in listi:
+        for j in range(26):
+            if AP[j] == l :
+                k.append(j)
+                print(AP[j])
+                break
+    for m in range(len(listi)):
+        mco += str(MC[k[int(m)]]) + ' '
+
+    return mco
+    
 Run = True
 while Run:
-    i = input('Alphabet : ')
-    i = i.upper()
-    for j in range(26):
-        if AP[j] == i :
-            k = j
-            break    
-    print('Morse Code for ',i, ' is ',MC[k])
+    i = input('String : ')
+    print('Morse Code '+A2M(i))
+    
     cont = input("Press Y to Check Again")
     Run =False
     if cont == 'Y' or cont =='y':
