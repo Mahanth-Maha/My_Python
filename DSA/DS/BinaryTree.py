907e9d82d74668cdd647661bd09d8f77b96f47cd
Mahanth-Maha <yallamahanth2002@gmail.com>
2022-08-05 14:40:28 +0530

    AVL :0

--- DSA/DS/BinaryTree.py
+++ DSA/DS/BinaryTree.py
@@ -1,7 +1,8 @@
 import time
 
-class TreeNode :
-    def __init__(self,d = 0,R=None,L=None) -> None:
+
+class TreeNode:
+    def __init__(self, d=0, R=None, L=None) -> None:
         self.rc = R
         self.d = d
         self.lc = L
@@ -10,14 +11,14 @@ class TreeNode :
 class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None
-    
+
     # def __init__(self,debug = False) -> None:
     #     if debug:
     #         self.root = TreeNode(30,TreeNode(50,TreeNode(60),TreeNode(40)),TreeNode(10,TreeNode(20),TreeNode(1)))
     #     else:
     #         self.__init__(self)
-    
-    def insert(self,data):
+
+    def insert(self, data):
         if self.root == None:
             self.root = TreeNode(data)
             return
@@ -36,8 +37,8 @@ class BinarySearchTree:
             '''Use this to see how many times it checks the tree before inserting '''
             # self.traverse_inorder()
             # time.sleep(1)
-    
-    def delete(self,target):
+
+    def delete(self, target):
         if self.BinarySearch(target) == False:
             return None
         if self.root.d == target:
@@ -49,10 +50,10 @@ class BinarySearchTree:
             right_min = self.delete(self.min_subtree(self.root.rc))
             self.root.d = right_min
             return temp
-        
+
         q = self.root
         parent = None
-        side  = None
+        side = None
         while q:
             if q.d == target:
                 # NO Child
@@ -90,26 +91,26 @@ class BinarySearchTree:
             elif q.d > target:
                 q = q.lc
                 side = 1
-            
+
         return None
 
-    def max_subtree(self,p):
+    def max_subtree(self, p):
         if p == None:
             return None
         while p.rc:
             p = p.rc
         return p.d
 
-    def min_subtree(self,p):
+    def min_subtree(self, p):
         if p == None:
             return None
         while p.lc:
             p = p.lc
         return p.d
 
-    def BinarySearch(self,target):
+    def BinarySearch(self, target):
         if target == None:
-            return False 
+            return False
         if self.root == None:
             return False
         q = self.root
@@ -125,84 +126,176 @@ class BinarySearchTree:
     def traverse_inorder(self):
         if self.root == None:
             return None
-        print('inorder :',end='')
+        print('inorder :', end='')
         self.inorder(self.root)
         print()
 
     def traverse_preorder(self):
         if self.root == None:
             return None
-        print('preorder :',end='')
+        print('preorder :', end='')
         self.preorder(self.root)
         print()
 
     def traverse_postorder(self):
         if self.root == None:
             return None
-        print('postorder :',end='')
+        print('postorder :', end='')
         self.postorder(self.root)
         print()
 
-    def inorder(self,q):
+    def inorder(self, q):
         if q:
             self.inorder(q.lc)
-            print(q.d,end=',')
+            print(q.d, end=',')
             self.inorder(q.rc)
-        
-    
-    def preorder(self,q):
+
+    def preorder(self, q):
         if q:
-            print(q.d,end=',')
+            print(q.d, end=',')
             self.preorder(q.lc)
             self.preorder(q.rc)
 
-    def postorder(self,q):
+    def postorder(self, q):
         if q:
             self.postorder(q.lc)
             self.postorder(q.rc)
-            print(q.d,end=',')
-    
-    def numberOfNodes(self,p):
+            print(q.d, end=',')
+
+    def numberOfNodes(self, p):
         if p == None:
             return 0
-        return  1 + self.numberOfNodes(p.rc) + self.numberOfNodes(p.lc)
-    
-    def height(self,p):
+        return 1 + self.numberOfNodes(p.rc) + self.numberOfNodes(p.lc)
+
+    def height(self, p):
         if p == None:
             return 0
-        return  1 + max(self.height(p.rc),self.height(p.lc))
+        return 1 + max(self.height(p.rc), self.height(p.lc))
+
 
 class AVLTree(BinarySearchTree):
-    
-    def balanceFactor(self,n):
+
+    def balanceFactor(self, n):
         if n == None:
             return 0
         if n.rc == None and n.lc == None:
             return 0
-        return self.height(n.lc) - self.height(n.lc)   
+        return self.height(n.lc) - self.height(n.lc)
 
-    def insert(self,data):
+    def insert(self, data):
         if self.root == None:
             self.root = TreeNode(data)
             return
         q = self.root
+        stack = []
         while q:
             if q.d < data:
                 if q.rc == None:
                     q.rc = TreeNode(data)
-                    return
+                    break
                 q = q.rc
             elif q.d > data:
                 if q.lc == None:
                     q.lc = TreeNode(data)
-                    return
+                    break
                 q = q.lc
+            stack.append(q)
+            # print([i.d for i in stack])
+        if len(stack) in (0,1) :
+            return
+        s2 = []
+        for _ in range(1,len(stack)+1):
+            w = self.balanceFactor(stack[-_])
+            if w in (1, 0, -1):
+                s2.append((stack[-_], w))
+            else:
+                s2.append((stack[-_], w))
+                break
+        if s2[-1][1] in (1, 0, -1):
+            return
+        if len(stack) == 0:
+            parent = self.root
+            rt = True
+        else:
+            parent = stack.pop()
+        if parent.rc == n1:
+            parent_side = 'L'
+        elif parent.lc == n1:
+            parent_side = 'R'
+        n1 = s2[-1][0]
+        n2 = s2[-2][0]
+        n3 = s2[-3][0]
+        rot = ''
+        if n1.rc == n2:
+            rot += 'R'
+        elif n1.lc == n2:
+            rot += 'L'
+        if n2.rc == n3:
+            rot += 'R'
+        elif n2.lc == n3:
+            rot += 'L'
+        if rot[0] == rot[1] and rot[0] == 'R':
+            #balance RR
+            if rt == False:
+                parent, n1, n2 = self.__RR(parent, n1, n2,parent_side)
+            else:
+                self.root, n1, n2 = self.__RR(parent, n1, n2,parent_side)
+            return
+        elif rot[0] == rot[1] and rot[0] == 'L':
+            #balance LL
+            if rt == False:
+                parent, n1, n2 = self.__LL(parent, n1, n2,parent_side)
+            else:
+                self.root, n1, n2 = self.__LL(parent, n1, n2,parent_side)
+            return
+        elif rot[0] == 'L' and rot[1] == 'R':
+            #balance LR
+            n1,n2,n3 = self.__LR(n1,n2,n3)
+            if rt == False:
+                parent, n1, n2 = self.__RR(parent, n1, n2,parent_side)
+            else:
+                self.root, n1, n2 = self.__RR(parent, n1, n2,parent_side)
+            return
+        elif rot[0] == 'R' and rot[1] == 'L':
+            #balance RL
+            n1,n2,n3 = self.__RL(n1,n2,n3)
+            if rt == False:
+                parent, n1, n2 = self.__LL(parent, n1, n2,parent_side)
+            else:
+                self.root, n1, n2 = self.__LL(parent, n1, n2,parent_side)
+            return
 
+    def __RR(self, parent, n1, n2,parent_side):
+        if parent_side == 'L':
+            parent.lc = n2
+        else:
+            parent.rc = n2
+        n1.rc = n2.lc
+        n2.lc = n1
+        return parent, n1, n2
 
-        
+    def __LL(self, parent, n1, n2,parent_side):
+        if parent_side == 'L':
+            parent.lc = n2
+        else:
+            parent.rc = n2
+        n1.lc = n2.rc
+        n2.rc = n1
+        return parent, n1, n2
+    
+    def __LR(self,n1,n2,n3):
+        n1.lc = n3
+        n2.rc = n3.lc
+        n3.lc = n2
+        return n1,n2,n3
+    def __RL(self,n1,n2,n3):
+        n1.rc = n3
+        n2.lc = n3.rc
+        n3.rc = n2
+        return n1,n2,n3
 
-            
-def main():
+def main_BS():
+    """ Helo """
     # BS = BinarySearchTree(debug=True)
     # BS.traverse_inorder()
     # BS.traverse_preorder()
@@ -212,8 +305,7 @@ def main():
     # BS.insert(25)
     # print(BS.BinarySearch(25))
     # BS.traverse_inorder()
-    
-    """Problem in BST without balancing """
+
     # BS = BinarySearchTree()
     # for i in range(1,10):
     #     BS.insert(i*100)
@@ -223,7 +315,7 @@ def main():
 
     # print(BS.max_subtree(BS.root))
     # print(BS.min_subtree(BS.root))
-    # BS.traverse_inorder()    
+    # BS.traverse_inorder()
     # print(BS.delete(34))
     # BS.traverse_inorder()
     # print(BS.delete(1))
@@ -242,11 +334,17 @@ def main():
     # BS.traverse_inorder()
     # print(BS.delete(50))
     # BS.traverse_inorder()
+
+
+def main():
     AVL = AVLTree()
-    AVL.traverse_inorder()
-    AVL.insert(20)
-    print(AVL.height(AVL.root))
-    AVL.traverse_inorder()
+    ins = [21,26,30,9,4,14,28,18,15,10,2,3,7]
+    for i in ins:
+        AVL.insert(i)
+        AVL.traverse_inorder()
+    ''' I Don't know why how what i did... but, why it worked for the first time that too AVL !!!!!!!!!
+    i mean it a redudant code still ! it shouldn't right xD '''
+
 
 if __name__ == '__main__':
     main()
